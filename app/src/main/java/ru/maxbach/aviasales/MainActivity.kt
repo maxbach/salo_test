f106dd398cf8cb2e98285a9cd89b7c1a921fafe6
Maxim Bachinsky <maxim.bachinsky@touchin.ru>
2020-09-24 23:33:55 +0300

    added simple animations between fragments

--- app/src/main/java/ru/maxbach/aviasales/MainActivity.kt
+++ app/src/main/java/ru/maxbach/aviasales/MainActivity.kt
@@ -2,10 +2,10 @@ package ru.maxbach.aviasales
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import ru.maxbach.aviasales.navigation.AnimatedNavigator
 import ru.maxbach.aviasales.navigation.Screens
 import ru.terrakok.cicerone.NavigatorHolder
 import ru.terrakok.cicerone.Router
-import ru.terrakok.cicerone.android.support.SupportAppNavigator
 import ru.touchin.roboswag.navigation_cicerone.CiceroneTuner
 import javax.inject.Inject
 
@@ -31,16 +31,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupNavigation() {
         lifecycle.addObserver(
-                CiceroneTuner(
-                        navigatorHolder,
-                        SupportAppNavigator(this, R.id.fragment_host)
-                )
+            CiceroneTuner(
+                navigatorHolder,
+                AnimatedNavigator(this, supportFragmentManager, R.id.fragment_host)
+            )
         )
     }
 
     private fun injectDi() {
         (application as App)
-                .component
-                .inject(this)
+            .component
+            .inject(this)
     }
 }
