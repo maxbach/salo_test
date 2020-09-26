a020c71b66d67e05741fff081aa5a92ea6346663
Максим Бачинский <mbachinskiyol@gmail.com>
2020-09-26 23:20:14 +0300

    added saving camera position on rotating

--- app/src/main/java/ru/maxbach/aviasales/presentation/map/MapFragment.kt
+++ app/src/main/java/ru/maxbach/aviasales/presentation/map/MapFragment.kt
@@ -2,44 +2,69 @@ package ru.maxbach.aviasales.presentation.map
 
 import android.os.Bundle
 import android.view.View
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.Marker
 import ru.maxbach.aviasales.R
 import ru.maxbach.aviasales.presentation.base.fragment.BaseFragment
 import ru.maxbach.aviasales.presentation.map.state.MapScreenMainState
-import ru.maxbach.aviasales.presentation.map.utils.*
+import ru.maxbach.aviasales.presentation.map.utils.addCityMarker
+import ru.maxbach.aviasales.presentation.map.utils.addPlaneMarker
+import ru.maxbach.aviasales.presentation.map.utils.drawPlanePath
+import ru.maxbach.aviasales.presentation.map.utils.focusMapOnRoute
+import ru.maxbach.aviasales.presentation.map.utils.movePlane
+import ru.maxbach.aviasales.presentation.map.utils.setup
 
 class MapFragment : BaseFragment<MapNavArgs, MapScreenViewModel>(R.layout.fragment_planes) {
 
     companion object {
+
+        private const val MAP_STATE_KEY_EXTRA = "MAP_STATE"
+
         fun create(navArgs: MapNavArgs) = MapFragment().apply {
             putNavArgs(navArgs)
         }
     }
 
-    private var planeMarker: Marker? = null
-
     private val viewModel by viewModel<MapScreenViewModel>()
+
+    private var planeMarker: Marker? = null
+    private var isCameraPositionRestored: Boolean = false
+
+    private lateinit var googleMap: GoogleMap
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.state.observe(viewLifecycleOwner) { state ->
-            getMapAsync { setupFirst(state) }
-        }
-
-        viewModel.planePath.observe(viewLifecycleOwner) { planePath ->
-            getMapAsync { drawPlanePath(requireContext(), planePath) }
-        }
-
         getMapAsync {
+            googleMap = this
+
+            savedInstanceState?.getParcelable<CameraPosition>(MAP_STATE_KEY_EXTRA)?.let { savedPosition ->
+                isCameraPositionRestored = true
+                moveCamera(CameraUpdateFactory.newCameraPosition(savedPosition))
+            }
+
+            viewModel.state.observe(viewLifecycleOwner) { state ->
+                setupFirst(state)
+            }
+
             viewModel.planePosition.observe(viewLifecycleOwner) { planePosition ->
                 planeMarker?.movePlane(planePosition) ?: run {
                     planeMarker = addPlaneMarker(planePosition)
                 }
             }
+
+            viewModel.planePath.observe(viewLifecycleOwner) { planePath ->
+                drawPlanePath(requireContext(), planePath)
+            }
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(MAP_STATE_KEY_EXTRA, googleMap.cameraPosition)
     }
 
     private fun GoogleMap.setupFirst(mainState: MapScreenMainState) {
@@ -48,13 +73,13 @@ class MapFragment : BaseFragment<MapNavArgs, MapScreenViewModel>(R.layout.fragme
         addCityMarker(requireContext(), mainState.cityFrom)
         addCityMarker(requireContext(), mainState.cityTo)
 
-        focusMapOnRoute(requireContext(), mainState.cityFrom.location, mainState.cityTo.location)
+        if (!isCameraPositionRestored) {
+            focusMapOnRoute(requireContext(), mainState.cityFrom.location, mainState.cityTo.location)
+        }
     }
 
     private fun getMapAsync(action: GoogleMap.() -> Unit) {
-        (childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment).getMapAsync {
-            it.action()
-        }
+        (childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment).getMapAsync { it.action() }
     }
 
 }
